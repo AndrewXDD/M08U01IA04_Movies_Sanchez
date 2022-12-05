b58e5cd71c18df441c7104ae9f8adad01b9170af
AndrewXDD <andreu.sanchez@sarria.salesians.cat>
2022-12-05 16:42:58 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'package:scooby_app/src/pages/home_page.dart';
+import 'package:scooby_app/src/pages/actor_detalle.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sanchez_Actors_TMDB',
+      initialRoute: '/',
+      routes: {
+        '/': (BuildContext context) => HomePage(),
+        'detalle': (BuildContext context) => ActorsDetalle(),
+      },
+    );
+  }
+}
